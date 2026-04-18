f6e4cc82f1f085654a4a17a378e6a6053a01b681
govarathan <113884318+govarathan@users.noreply.github.com>
2026-04-18 19:25:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/helpline_website_new1/',
-  plugins: [react()]
+  plugins: [react()],
+  base: '/helpline_website_new1/', // ADD THIS LINE
 })
